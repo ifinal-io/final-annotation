2cca48b7a97db879b29e7f06ac4e8f835cb29f15
likly <likly@ilikly.com>
2021-04-20 10:09:47 +0800

    update BetweenValue

--- final-annotation-query/src/main/java/org/ifinal/finalframework/query/BetweenValue.java
+++ final-annotation-query/src/main/java/org/ifinal/finalframework/query/BetweenValue.java
@@ -1,7 +1,10 @@
 package org.ifinal.finalframework.query;
 
+import org.springframework.lang.Nullable;
+
 import org.ifinal.finalframework.query.annotation.Between;
 import org.ifinal.finalframework.query.annotation.NotBetween;
+import org.ifinal.finalframework.query.condition.BetweenCondition;
 
 import java.io.Serializable;
 
@@ -10,10 +13,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
+ * The value holder which have one {@code min} value and {@code max} value for {@code between} value.
+ *
  * @author likly
  * @version 1.0.0
  * @see Between
  * @see NotBetween
+ * @see BetweenCondition
  * @since 1.0.0
  */
 @Data
@@ -23,8 +29,10 @@ public final class BetweenValue<T> implements Serializable {
 
     private static final long serialVersionUID = 6194662646358531082L;
 
+    @Nullable
     private T min;
 
+    @Nullable
     private T max;
 
 }
