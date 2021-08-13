ae651283eb7667ed1b43e81deac2d5c446522241
likly <likly@ilikly.com>
2021-08-13 16:10:11 +0800

    add IRepository.

--- final-annotation-core/src/main/java/org/ifinalframework/core/IRepository.java
+++ final-annotation-core/src/main/java/org/ifinalframework/core/IRepository.java
@@ -15,43 +15,38 @@
 
 package org.ifinalframework.core;
 
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
-
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * IRepository.
  *
  * @author likly
- * @version 1.2.1
- * @since 1.2.1
+ * @version 1.2.2
+ * @since 1.2.2
  */
 public interface IRepository<I extends Serializable, T extends IEntity<I>> {
 
-    int insert(@Nullable String table, @Nullable Class<?> view, boolean ignore, @NonNull Collection<T> entities);
+    int insert(Map<String, Object> params);
 
-    int replace(@Nullable String table, @Nullable Class<?> view, @NonNull Collection<T> entities);
+    int replace(Map<String, Object> params);
 
-    int save(@Nullable String table, @Nullable Class<?> view, @NonNull Collection<T> entities);
+    int save(Map<String, Object> params);
 
-    int update(@Nullable String table, @Nullable Class<?> view,
-        @Nullable T entity, @Nullable IUpdate update, boolean selective,
-        @Nullable Collection<I> ids, @Nullable IQuery query);
+    int update(Map<String, Object> params);
 
-    int delete(@Nullable String table, @Nullable Collection<I> ids, @Nullable IQuery query);
+    int delete(Map<String, Object> params);
 
-    List<T> select(@Nullable String table, @Nullable Class<?> view, @Nullable Collection<I> ids,
-        @Nullable IQuery query);
+    List<T> select(Map<String, Object> params);
 
-    T selectOne(@Nullable String table, @Nullable Class<?> view, @Nullable I id, @Nullable IQuery query);
+    List<I> selectIds(Map<String, Object> params);
 
-    List<I> selectIds(@Nullable String table, @NonNull IQuery query);
+    T selectOne(Map<String, Object> params);
 
-    long selectCount(@Nullable String table, @Nullable Collection<I> ids, @Nullable IQuery query);
+    long selectCount(Map<String, Object> params);
 
-    void truncate(@Nullable String table);
+    void truncate(Map<String, Object> params);
+
 
 }
