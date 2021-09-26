4c09818a94265893afd0181e8ea1e979171eb8f0
likly <likly@ilikly.com>
2021-09-26 14:48:54 +0800

    update checkstyle

--- final-annotation-query/src/main/java/org/ifinalframework/query/annotation/Order.java
+++ final-annotation-query/src/main/java/org/ifinalframework/query/annotation/Order.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +11,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.ifinalframework.query.annotation;
@@ -29,6 +27,7 @@ import java.lang.annotation.Target;
  * @version 1.0.0
  * @see org.ifinalframework.core.Orderable
  * @since 1.0.0
+ * @deprecated replaced by {@link org.ifinalframework.core.Orderable}
  */
 
 @Deprecated
@@ -41,9 +40,9 @@ public @interface Order {
     int order() default 0;
 
     String[] value() default {
-        "<if test=\"${value} != null\">",
-        "   ${column} \\${${value}},",
-        "</if>"
+            "<if test=\"${value} != null\">",
+            "   ${column} \\${${value}},",
+            "</if>"
     };
 
 }
