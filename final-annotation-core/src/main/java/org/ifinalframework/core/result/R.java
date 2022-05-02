644124322b612243ca3540554dadf6b0b9d3ae03
ilikly <ifinal.io@qq.com>
2022-05-02 09:03:48 +0800

    update

--- final-annotation-core/src/main/java/org/ifinalframework/core/result/R.java
+++ final-annotation-core/src/main/java/org/ifinalframework/core/result/R.java
@@ -18,43 +18,40 @@ package org.ifinalframework.core.result;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+import org.ifinalframework.core.ResponseStatus;
+
+import lombok.experimental.UtilityClass;
+
 /**
  * @author ilikly
  * @version 1.0.0
  * @since 1.0.0
  */
-public final class R {
-
-    private static final Integer SUCCESS_CODE = 0;
-
-    private static final String SUCCESS_MESSAGE = "success";
-
-    private R() {
-    }
-
+@UtilityClass
+public class R {
     public static <T> Result<T> success() {
         return success(null);
     }
 
-    public static <T> Result<T> success(final T data) {
+    public static <T> Result<T> success(T data) {
 
-        return new Result<>(SUCCESS_CODE, SUCCESS_MESSAGE, data);
+        return new Result<>(ResponseStatus.SUCCESS.getCode(), ResponseStatus.SUCCESS.getDesc(), data);
     }
 
-    public static <T> Result<T> success(final @NonNull String code,
-        final @NonNull String message,
-        final @Nullable T data) {
+    public static <T> Result<T> success(@NonNull String code,
+                                        @NonNull String message,
+                                        @Nullable T data) {
 
-        return new Result<>(SUCCESS_CODE, SUCCESS_MESSAGE, code, message, data);
+        return new Result<>(ResponseStatus.SUCCESS.getCode(), ResponseStatus.SUCCESS.getDesc(), code, message, data);
     }
 
-    public static <T> Result<T> failure(final @NonNull Integer status, final @NonNull String description,
-        final @NonNull String code, final @NonNull String message) {
+    public static <T> Result<T> failure(@NonNull Integer status, @NonNull String description,
+                                        @NonNull String code, @NonNull String message) {
 
         return new Result<>(status, description, code, message);
     }
 
-    public static <T> Result<T> failure(final @NonNull Integer status, final @NonNull String message) {
+    public static <T> Result<T> failure(@NonNull Integer status, @NonNull String message) {
 
         return new Result<>(status, message);
     }
