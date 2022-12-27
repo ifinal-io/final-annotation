415866b52208ee80a8ef6ce2a6be1340912c25b5
ilikly <ifinal.io@qq.com>
2022-12-27 16:16:40 +0800

    remove ReferenceMode.java

--- final-annotation-data/src/main/java/org/ifinalframework/data/annotation/Reference.java
+++ final-annotation-data/src/main/java/org/ifinalframework/data/annotation/Reference.java
@@ -17,7 +17,11 @@
 
 package org.ifinalframework.data.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 
 import org.springframework.core.annotation.AliasFor;
@@ -46,8 +50,6 @@ public @interface Reference {
 
     @AliasFor(annotation = Column.class)
     String name() default "";
-
-    ReferenceMode mode() default ReferenceMode.SIMPLE;
 
     String[] properties();
 
