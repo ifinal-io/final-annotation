e7e7fe69628bcbb6d7f6bcd4a387e332a16cb2c0
ilikly <ifinal.io@qq.com>
2022-12-27 13:02:10 +0800

    update

--- final-annotation-data/src/main/java/org/ifinalframework/data/spi/PreInsertConsumer.java
+++ final-annotation-data/src/main/java/org/ifinalframework/data/spi/PreInsertConsumer.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2020-2021 the original author or authors.
- *
+ * Copyright 2020-2022 the original author or authors.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,24 +11,19 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-package org.ifinalframework.core;
+package org.ifinalframework.data.spi;
 
-import java.util.List;
+import org.springframework.lang.NonNull;
 
 /**
- * Orderable.
+ * PreInsertConsumer.
  *
  * @author ilikly
- * @version 1.0.0
- * @since 1.0.0
+ * @version 1.4.2
+ * @since 1.4.2
  */
-public interface Orderable {
-
-    List<String> getOrders();
-
-    void setOrders(List<String> orders);
-
+public interface PreInsertConsumer<T, U> {
+    void accept(@NonNull T entity, @NonNull U user);
 }
