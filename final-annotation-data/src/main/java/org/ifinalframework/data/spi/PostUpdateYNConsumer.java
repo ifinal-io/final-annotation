9c59b53e8f61f2b4620377c262a44388a64f3218
ilikly <ifinal.io@qq.com>
2023-01-05 23:48:26 +0800

    add some spi

---
+++ final-annotation-data/src/main/java/org/ifinalframework/data/spi/PostUpdateYNConsumer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020-2023 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ifinalframework.data.spi;
+
+import org.springframework.lang.NonNull;
+
+import org.ifinalframework.data.annotation.YN;
+
+/**
+ * PostUpdateYNConsumer.
+ *
+ * @author ilikly
+ * @version 1.4.3
+ * @see PreUpdateYnValidator
+ * @see AfterReturnUpdateYnConsumer
+ * @since 1.4.3
+ */
+@FunctionalInterface
+public interface PostUpdateYNConsumer<T, U> {
+    void accept(@NonNull T entity, @NonNull YN yn, @NonNull U user);
+}
